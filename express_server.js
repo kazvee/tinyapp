872e5a5b321336c94005576c5d3188a1eb26416f
kazvee <kayvee123@gmail.com>
2023-06-24 10:05:42 -0600

    Clean up code & comments to improve readability

--- express_server.js
+++ express_server.js
@@ -1,25 +1,31 @@
+//================
+//  DEPENDENCIES
+//================
+
 // Import express library
 const express = require("express");
-
 // Import cookie-parser
 const cookieParser = require('cookie-parser');
-
 // Define app as an instance of express
 const app = express();
-
-// Define base URL as http://localhost:8080
+// Define base URL with port number on which the server will listen as http://localhost:8080
 const PORT = 8080;
 
-// Configure app to use ejs as the templating engine
-app.set("view engine", "ejs");
+//===========
+//  OBJECTS
+//===========
 
-// Object containing shortened URLs
+// Object containing shortened URLs as key-value pairs where key is `id` for the Short URL ID and value is `longURL` for the corresponding Long URL
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// Generate a string of 6 random alphanumeric characters (a-z, A-Z, and 0-9)
+//=============
+//  FUNCTIONS
+//=============
+
+// Generate string of 6 random alphanumeric characters (a-z, A-Z, and 0-9) to be used as Short URL ID
 const generateRandomString = () => {
   let result = '';
   let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
@@ -32,15 +38,27 @@ const generateRandomString = () => {
   return result;
 };
 
-// Middleware to parse the request body data from a Buffer into human-readable string
-app.use(express.urlencoded({ extended: true }));
+//==============
+//  MIDDLEWARE
+//==============
 
-// Middleware to parse cookie requests into human-readable details
+// Configure app to use ejs as the templating engine to render pages dynamically
+app.set("view engine", "ejs");
+// Parse request body data from Buffer into human-readable string and makes the values accessible
+app.use(express.urlencoded({ extended: true }));
+// Parse cookie requests into human-readable details and makes the values accessible
 app.use(cookieParser());
+
+//==========
+//  ROUTES
+//==========
+
+//==============
+//  GET ROUTES
+//==============
 
 // Route handler for root path '/'
 app.get("/", (req, res) => {
-
   // Send response to client
   res.send("Hello!");
 });
@@ -51,14 +69,12 @@ app.get("/urls/new", (req, res) => {
     // Pass in the username (using the cookie value) to the url_index template
     username: req.cookies["username"],
   };
-
   // Find the urls_new template and send it to the browser
   res.render("urls_new", templateVars);
 });
 
 // Route for /urls/:id endpoint (note that :id is a route parameter)
 app.get("/urls/:id", (req, res) => {
-
   // Object to pass data for a single URL to the template
   const templateVars = {
     // Pass in the username (using the cookie value) to the url_index template
@@ -66,120 +82,104 @@ app.get("/urls/:id", (req, res) => {
     id: req.params.id,
     longURL: urlDatabase[req.params.id]
   };
-
   // Pass data for a single URL to the template
   res.render("urls_show", templateVars);
 });
 
 // Route for /u/:id endpoint (note that :id is a route parameter)
 app.get("/u/:id", (req, res) => {
-
   // Get the longURL value associated with the Short URL (id) in the urlDatabase object
   const longURL = urlDatabase[req.params.id];
-
   // Redirect user to the longURL website
   res.redirect(longURL);
 });
 
 // Route for /urls.json endpoint
 app.get("/urls.json", (req, res) => {
-
   // Send urlDatabase object as a JSON response to the client
   res.json(urlDatabase);
 });
 
 // Route for /urls endpoint
 app.get("/urls", (req, res) => {
-
   // Object to pass data to the template
   const templateVars = {
     // Pass in the username (using the cookie value) to the url_index template
     username: req.cookies["username"],
     urls: urlDatabase
   };
-
   // Pass URL data to the template
   res.render("urls_index", templateVars);
 });
 
 // Route for /hello endpoint
 app.get("/hello", (req, res) => {
-
   // Send response with HTML content to client
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// POST route for /urls endpoint to receive new URL form submission
-app.post("/urls", (req, res) => {
+//===============
+//  POST ROUTES
+//===============
 
+// Route for /urls endpoint to receive new URL form submission
+app.post("/urls", (req, res) => {
   // Set longURL to be the POST request body
   const longURL = req.body.longURL;
-
   // Use generateRandomString function to generate short URL ID
   const id = generateRandomString();
-
   // Assign longURL as the value of the newly-generated Short URL ID key in the urlDatabase object
   urlDatabase[id] = longURL;
-
   // Redirect user to /urls/:id
   res.redirect(`/urls/${id}`);
 });
 
-// POST route for /urls/:id/delete to handle short URL ID deletion
+// Route for /urls/:id/delete to handle short URL ID deletion
 app.post(`/urls/:id/delete`, (req, res) => {
-
   // Set Short URL ID to be the POST request body
   const id = req.params.id;
-
   // Delete the specified Short URL ID
   delete urlDatabase[id];
-
   // Redirect user to /urls
   res.redirect(`/urls`);
 });
 
-// POST route for /urls/:id to handle editing of Short URL ID details
+// Route for /urls/:id to handle editing of Short URL ID details
 app.post(`/urls/:id`, (req, res) => {
-
   // Get the id from the route parameter
   const id = req.params.id;
-
   // Assign longURL value to be the editURL value received from the POST request body
   const longURL = req.body.newURL;
-
   // Update the longUrl value in the database with the new value
   urlDatabase[id] = longURL;
-
   // Redirect user to /urls
   res.redirect(`/urls`);
 });
 
-// POST route for /login
+// Route to handle user Login
 app.post('/login', (req, res) => {
-
-  // Get the username from the request body
+  // Get username from request body
   const username = req.body.username;
-
-  // Set a cookie named username to the value for username submitted in the request body
+  // Set cookie named `username` to the value for username submitted in request body
   res.cookie('username', username);
-
   // Redirect user to /urls
   res.redirect('/urls');
 });
 
-// POST route for /logout
+// Route to handle user Logout
 app.post('/logout', (req, res) => {
-
   // Clear the `username` cookie
   res.clearCookie('username');
-
   // Redirect user to /urls
   res.redirect('/urls');
 });
+
+//================
+// END OF ROUTES
+//================
 
 // Start express server and listen on specified port
 app.listen(PORT, () => {
-
   // Confirm port number in console message
   console.log(`App listening on port ${PORT}! 😀`);
 });
