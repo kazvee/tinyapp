fba6f744b56b5ced11703d7aabd012bd83a98d63
kazvee <kayvee123@gmail.com>
2023-06-29 20:54:13 -0600

    Move generateRandomString to helpers.js module

--- express_server.js
+++ express_server.js
@@ -17,7 +17,7 @@ const PORT = 8080;
 // Import bcryptjs to hash user passwords
 const bcrypt = require('bcryptjs');
 // Import helper function
-const getUserByEmail = require('./helpers');
+const { getUserByEmail, generateRandomString } = require('./helpers');
 
 //===========
 //  OBJECTS
@@ -60,19 +60,6 @@ const htmlBodyEnd = `</body></html>\n`;
 //=============
 //  FUNCTIONS
 //=============
-
-// Helper function to generate new Short URL ID from a string of 6 random alphanumeric characters (a-z, A-Z, and 0-9)
-const generateRandomString = () => {
-  let result = '';
-  let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let length = characters.length;
-  // Loop through the characters string to generate a length of 6
-  for (let i = 0; i < 6; i++) {
-    // Select a random character from the characters string
-    result += characters.charAt(Math.floor(Math.random() * length));
-  }
-  return result;
-};
 
 // Helper function to return the URLs belonging to the current user
 // Takes in 'id' as a parameter and returns URLs where 'userID' is equal to the 'id' of the user currently logged in
--- helpers.js
+++ helpers.js
@@ -13,4 +13,19 @@ const getUserByEmail = (email, database) => {
   return null;
 };
 
-module.exports = getUserByEmail;
+// Helper function to generate new Short URL ID from a string of 6 random alphanumeric characters (a-z, A-Z, and 0-9)
+const generateRandomString = () => {
+  let result = '';
+  let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let length = characters.length;
+  // Loop through the characters string to generate a length of 6
+  for (let i = 0; i < 6; i++) {
+    // Select a random character from the characters string
+    result += characters.charAt(Math.floor(Math.random() * length));
+  }
+  return result;
+};
+
+
+
+module.exports = { getUserByEmail, generateRandomString };
